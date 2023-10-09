6de943d77440fed1cba27d253631b67fd6aa87e1
kweonminsung <kevin136583@gmail.com>
2023-10-10 00:20:38 +0900

    feat: 2023/10/9

---
+++ 15650/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    
+    vector<int> arr;
+    for(int i = 1; i <= n; i++) arr.push_back(i);
+
+    vector<int> arr2;
+    for(int i = 0 ; i < m; i++) arr2.push_back(1);
+    for(int i = m; i < n; i++) arr2.push_back(0);
+
+    do {
+        for(int i = 0; i < n; i++) {
+            if(arr2[i]) cout << arr[i] << " ";
+        }
+        cout << "\n";
+    } while(prev_permutation(arr2.begin(), arr2.end()));
+
+    
+}
