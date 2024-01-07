9edb5020c3cf7644aab83b78abc447fa10d57159
kweonminsung <kevin136583@gmail.com>
2024-01-07 20:14:40 +0900

    feat: 2024/1/3

---
+++ 10799/main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  string s;
+  cin >> s;
+
+  int cnt = 0;
+
+  stack<char> S;
+
+  for (int i = 0; i < s.size(); i++)
+  {
+    if (s[i] == '(')
+    {
+      cnt++;
+      S.push(s[i]);
+    }
+    else
+    {
+      if (S.top() == '(')
+      {
+        if (s[i - 1] == '(')
+        {
+          S.pop();
+          cnt += S.size() - 1;
+          continue;
+        }
+        S.pop();
+        continue;
+      }
+
+      S.push(s[i]);
+    }
+  }
+
+  cout << cnt;
+}
---
+++ 1158/main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+queue<int> Q;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, k;
+  cin >> n >> k;
+
+  for (int i = 1; i <= n; i++)
+  {
+    Q.push(i);
+  }
+
+  cout << "<";
+  while (!Q.empty())
+  {
+    int toPop = k % Q.size() == 0 ? k : k % Q.size();
+    for (int i = 0; i < toPop - 1; i++)
+    {
+      Q.push(Q.front());
+      Q.pop();
+    }
+
+    cout << Q.front();
+    if (Q.size() == 1)
+      cout << ">";
+    else
+      cout << ", ";
+    Q.pop();
+  }
+}
