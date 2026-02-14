420d7dc9032e7b0281604670c385025f74b990e1
kweonminsung <kevin136583@gmail.com>
2026-02-14 19:11:01 +0900

    feat: 2026/2/13

---
+++ 8394/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int dp[10000001];
+
+int main() {
+    cin >> n;
+
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 3;
+
+    for(int i = 4; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10;
+    }
+
+    cout << dp[n];
+}
